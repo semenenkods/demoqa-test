f72d67b6727043a60f338c6389e52155befe4701
semenenkods <semenenkods@gmail.com>
2023-09-17 21:58:46 -0600

    Upload img, address, State, City selected. Window size "640x480", submit form.

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,15 +1,23 @@
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+
 public class SearchTests {
     @Test
     void successfulSearchTest() {
+
+        Configuration.browserSize = "640x480";
+        Configuration.holdBrowserOpen = true;
         open("https://demoqa.com/automation-practice-form");
+
+
         $("#firstName").setValue("Test");
         $("#lastName").setValue("First");
         $("#userEmail").setValue("testfirst@gmail.com");
+        $("#userEmail").scrollIntoView(true);//for small res-n
         $(byText("Female")).click();
         $("#userNumber").setValue(("1234567890"));
         $("#dateOfBirthInput").click();
@@ -19,11 +27,23 @@ public class SearchTests {
         $(byText("13")).click();
         $("#subjectsInput").setValue("m");
         $(byText("Maths")).click();
+        $(byText("Maths")).scrollIntoView(true);//for small res-n
 
         $(byText("Sports")).click();
-        $(byText("Select picture")).click();
 
+        //Upload img
+        //$(byText("Select picture")).click();///
+        $("#uploadPicture").uploadFromClasspath("testPicture.jpg");
 
+        $("#currentAddress").setValue("my current address");
+        $("#currentAddress").scrollIntoView(true);//for small res-n
 
+       $(byText("Select State")).click();
+       $(byText("NCR")).click();
+
+       $(byText("Select City")).click();
+       $(byText("Delhi")).click();
+
+       $("#submit").click();
     }
 }
