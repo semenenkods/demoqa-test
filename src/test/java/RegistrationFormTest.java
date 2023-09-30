940d5bc9e10df0b69b4969c3b9089eed4dc6e54a
semenenkods <semenenkods@gmail.com>
2023-09-30 16:37:36 -0600

    Verification, banner & footer removing, browser configuration changed.

--- src/test/java/RegistrationFormTest.java
+++ src/test/java/RegistrationFormTest.java
@@ -1,23 +1,27 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
-
-public class SearchTests {
+public class RegistrationFormTest {
     @Test
     void successfulSearchTest() {
 
-        Configuration.browserSize = "640x480";
-        Configuration.holdBrowserOpen = true;
+        //Browser configuration
+        Configuration.browserSize = "1920x1080";
+        //Configuration.holdBrowserOpen = true;
+
         open("https://demoqa.com/automation-practice-form");
 
+        //Banner & footer removing
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
 
+        //Entering data
         $("#firstName").setValue("Test");
         $("#lastName").setValue("First");
         $("#userEmail").setValue("testfirst@gmail.com");
-        $("#userEmail").scrollIntoView(true);//for small res-n
         $(byText("Female")).click();
         $("#userNumber").setValue(("1234567890"));
         $("#dateOfBirthInput").click();
@@ -27,23 +31,17 @@ public class SearchTests {
         $(byText("13")).click();
         $("#subjectsInput").setValue("m");
         $(byText("Maths")).click();
-        $(byText("Maths")).scrollIntoView(true);//for small res-n
-
         $(byText("Sports")).click();
-
-        //Upload img
-        //$(byText("Select picture")).click();///
         $("#uploadPicture").uploadFromClasspath("testPicture.jpg");
-
         $("#currentAddress").setValue("my current address");
-        $("#currentAddress").scrollIntoView(true);//for small res-n
+        $(byText("Select State")).click();
+        $(byText("NCR")).click();
+        $(byText("Select City")).click();
+        $(byText("Delhi")).click();
+        $("#submit").click();
 
-       $(byText("Select State")).click();
-       $(byText("NCR")).click();
-
-       $(byText("Select City")).click();
-       $(byText("Delhi")).click();
-
-       $("#submit").click();
+        //Verification
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Test First"), text("testfirst@gmail.com"), text("1234567890"), text("my current address"));
     }
 }
